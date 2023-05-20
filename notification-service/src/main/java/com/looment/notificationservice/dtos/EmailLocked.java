8a5505dedf32319d6bdda1f0fe4810c186b17cdd
fahmy <fahmy.malik@doku.com>
2023-05-20 17:56:11 +0700

    LOOMENT NOTIFICATION-SERVICE: Init kafka and email notification service done

---
+++ notification-service/src/main/java/com/looment/notificationservice/dtos/EmailLocked.java
@@ -0,0 +1,27 @@
+package com.looment.notificationservice.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmailLocked implements Serializable {
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("username")
+    private String username;
+    @Override
+    public String toString() {
+        return "{" +
+                "\"email\": \"" + email + "\"," +
+                "\"username\": \"" + username + "\"" +
+                "}";
+    }
+}
---
+++ notification-service/src/main/java/com/looment/notificationservice/dtos/EmailOTP.java
@@ -0,0 +1,32 @@
+package com.looment.notificationservice.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmailOTP implements Serializable {
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("otp")
+    private String otp;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"email\": \"" + email + "\"," +
+                "\"username\": \"" + username + "\"," +
+                "\"otp\": \"" + otp + "\"" +
+                "}";
+    }
+
+}
